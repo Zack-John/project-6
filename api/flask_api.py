d2a7938374f980c7334e2a207a2cd035c7dc17d8
Zack <johnsonzack94@gmail.com>
2023-03-08 17:34:16 -0800

    api working (with strings)

--- api/flask_api.py
+++ api/flask_api.py
@@ -2,30 +2,27 @@
 Brevets RESTful API
 """
 import os
+import logging
 from flask import Flask
 from flask_restful import Api
 from mongoengine import connect
 
-import logging
-
-# TODO: finish resources.brevet before i do this part
-# from resources.brevet import Brevet
-from resources.brevets import Brevets
+from resources.brevet import BrevetResource
+from resources.brevets import BrevetsResource
 
 # Connect MongoEngine to mongodb
 connect(host=f"mongodb://{os.environ['MONGODB_HOSTNAME']}:27017/brevetsdb")
 
 # Start Flask app and Api here:
 app = Flask(__name__)
+app.debug = True if "DEBUG" not in os.environ else os.environ["DEBUG"]
+port_num = True if "PORT" not in os.environ else os.environ["PORT"]
+app.logger.setLevel(logging.DEBUG)
 api = Api(app)
 
 # Bind resources to paths here:
-# api.add_resource(Brevet)                  # TODO: get urls from flask_brevets.py
-# api.add_resource(Brevets, "/api/______")  # TODO: get urls from flask_brevets.py
+api.add_resource(BrevetResource, "/api/brevet/<id>")       
+api.add_resource(BrevetsResource, "/api/brevets")
 
 if __name__ == "__main__":
-    # Run flask app normally
-    # Read DEBUG and PORT from environment variables.
-
-    # print("Opening for global access on port {}".format(CONFIG.PORT))
-    app.run(port=os.environ["PORT"], host="0.0.0.0")
+    app.run(port=port_num, host="0.0.0.0")
